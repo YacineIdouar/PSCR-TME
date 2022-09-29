07fec7fa1b838bc664f79eed784b3de9b980f593
YacineIdouar <yacine.scomp@gmail.com>
2022-09-29 08:45:32 +0200

    Correction de toutes les erreurs

--- TME1/exo1/src/main.cpp
+++ TME1/exo1/src/main.cpp
@@ -6,11 +6,12 @@
 int main () {
 
 	std::string abc = "abc";
-	char * str = new char [3];
+	char * str = new char [4];
 	str[0] = 'a';
 	str[1] = 'b';
 	str[2] = 'c';
-	size_t i = 0;
+	str[3] = '\0';
+	int i = 0;
 
 	if (! strcmp (str, abc.c_str())) {
 		std::cout << "Equal !";
@@ -28,10 +29,7 @@ int main () {
 		std::cout << "elt " << i << ": " << list[i] << std::endl;
 	}
 
-	// liberer les char de la chaine
-	for (char *cp = str ; *cp ; cp++) {
-		delete cp;
-	}
+	
 	// et la chaine elle meme
 	delete[] str;
 
